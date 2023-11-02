fee729a861e833d73f89fa61453671b93236f6ad
MrStive <stivedongawis@gmail.com>
2023-11-02 03:03:44 +0100

    feat:picture-1: configuration

---
+++ src/main/java/com/dongawis/pctiture2/Pctiture2Application.java
@@ -0,0 +1,13 @@
+package com.dongawis.pctiture2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pctiture2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pctiture2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongawis/pctiture2/Pctiture2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongawis.pctiture2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pctiture2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
